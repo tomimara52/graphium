072ea956d9b8ac69f7610326b3c3f2451f2e63a4
Tomás Maraschio <tomas.maraschio@gmail.com>
2022-07-11 16:22:15 -0300

    enable user to input graph to display interactively

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
 module Main where
 
-import GraphiumLib
+import Control.Monad
+import qualified Data.Map as M
+import qualified GraphiumLib as G
 
-main :: IO () 
-main = GraphiumLib.start
+main :: IO ()
+--main = GraphiumLib.start
+main = do
+  putStrLn "How many vertices do you want your graph to have?"
+  nStr <- getLine
+  let n = read nStr :: Int
+      vertices = [1..n]
+  putStrLn "Your graph will have the following vertices:"
+  print vertices
+  graphList <- forM vertices (\v -> do
+    putStrLn $ "\nWhat vertices should " ++ show v ++ " be adjacent to?"
+    putStrLn "Put the vertices between spaces, like so: 1 3 5"
+    adjList <- getLine >>= (return . map head . words)
+    return (head $ show v, adjList)
+    )
+  let graph = M.fromList graphList
+  putStrLn "\nThis is your graph:"
+  putStr $ G.adjTable graph
+  G.start graph
