95efb380631ed34f4a929a22ff0709afbd551309
Tomás Maraschio <tomas.maraschio@gmail.com>
2022-08-03 17:21:19 -0300

    delete old commented code

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,6 @@ import qualified Data.Map as M
 import qualified GraphiumLib as G
 
 main :: IO ()
---main = GraphiumLib.start
 main = do
   putStrLn "Do you want to load a graph from a file? (y/n)"
   answer <- getLine
@@ -39,14 +38,3 @@ main = do
         writeFile ("saved-graphs/" ++ graphName) $ G.adjTable graph
         G.start graph
       else G.startEmpty
-    {--
-      putStrLn "Will you want to save the graph for future sessions? (y/n)"
-      answer2 <- getLine
-      if answer2 == "y"
-        then do
-          putStrLn "What do you want the file to be named?"
-          newFilename <- getLine
-          writeFile ("saved-graphs/" ++ newFilename) $ G.adjTable graph
-          G.startEmpty
-        else do
---}
