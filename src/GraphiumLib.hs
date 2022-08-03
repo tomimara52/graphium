8a560d7d5f022c8f756f64003cdb2c2182da73fa
Tomás Maraschio <tomas.maraschio@gmail.com>
2022-08-03 16:28:28 -0300

    startEmpty, create graph interactively by clicking to put and join vertices

--- src/GraphiumLib.hs
+++ src/GraphiumLib.hs
@@ -17,26 +17,58 @@ startFromFile filename = do
   let gr = tableToGraph contents
   start gr
 
+startEmpty :: IO ()
+startEmpty = play window white 60 initialWorldEmpty render handle update
+
 window :: Display 
 window = InWindow "Graphium" (300, 300) (0, 0) 
 
 render :: World -> Picture 
-render world = pictures [graphEdges gr, graphVertices gr]
+render world = pictures [graphEdges gr, graphVertices gr, displayMode world]
   where gr = graphState world
 
 handle :: Event -> World -> World
-handle (EventKey (MouseButton LeftButton) Down _ (x, y)) world = 
-  if L.null clickedResult
-  then world 
-  else world { isClicked = True, draggedV = unjust clickedResult }
-    where clickedResult = clickedV x y $ graphState world
-          unjust (Just x) = x
+handle (EventKey (MouseButton LeftButton) Down _ (x, y)) world
+  | putVerticesMode world = createVertex x y world
+  | putEdgesMode world = joinVertices x y world
+  | otherwise = 
+    if L.null clickedResult
+    then world 
+    else world { isClicked = True, draggedV = unjust clickedResult }
+      where clickedResult = clickedV x y $ graphState world
+            unjust (Just x) = x
 handle (EventKey (MouseButton LeftButton) Up _ (x, y)) world = world { isClicked = False }
 handle (EventMotion (x, y)) world =
   if isClicked world 
   then moveV x y world
   else world
+handle (EventKey (SpecialKey KeySpace) Down _ _) world
+  | putVerticesMode world = world { putVerticesMode = False, putEdgesMode = True }
+  | putEdgesMode world = world { putEdgesMode = False }
+  | otherwise = world
 handle _ world = world
+
+createVertex :: Float -> Float -> World -> World
+createVertex x y world = world { graphState = newGr }
+  where n = head $ show $ L.genericLength $ M.keys actualGr
+        actualGr = graphState world
+        newGr = M.insert n ("", (x, y)) actualGr
+
+joinVertices :: Float -> Float -> World -> World
+joinVertices x y world 
+  | L.null clickedResult = world
+  | fst $ toJoinV world =
+      let a = snd $ toJoinV world
+          b = unjust clickedResult
+          addEdges (v, _) (adj, pos) = (adj ++ v, pos)
+          newGr = M.insertWith addEdges a ([b], (0,0)) $ M.insertWith addEdges b ([a], (0,0)) (graphState world)
+      in if a == b
+         then world
+         else world { graphState = newGr, toJoinV = (False, 'x') } 
+  | otherwise = world { toJoinV = (True, unjust clickedResult) }
+  where clickedResult = clickedV x y $ graphState world
+        unjust (Just x) = x
+
 
 update :: Float -> World -> World 
 update _ world = world
@@ -58,11 +90,29 @@ data World = World
   { graphState :: GraphState 
   , isClicked :: Bool 
   , draggedV :: Char 
+  , putVerticesMode :: Bool
+  , putEdgesMode :: Bool
+  , toJoinV :: (Bool, Char) 
   }
 
 initialWorld :: Graph -> World 
-initialWorld gr = World { graphState = grState, isClicked = False, draggedV = 'x'}
+initialWorld gr = World { graphState = grState
+                        , isClicked = False
+                        , draggedV = 'x'
+                        , putVerticesMode = False
+                        , putEdgesMode = False
+                        , toJoinV = (False, 'x')
+                        }
   where grState = createGraphState gr
+
+initialWorldEmpty :: World
+initialWorldEmpty = World { graphState = M.fromList []
+                          , isClicked = False
+                          , draggedV = 'x'
+                          , putVerticesMode = True
+                          , putEdgesMode = False
+                          , toJoinV = (False, 'x')
+                          }
 
 type GraphState = M.Map Char ([Char], (Float, Float))
 
@@ -92,6 +142,12 @@ graphEdges :: GraphState -> Picture
 graphEdges gr = pictures $ L.concatMap mkEdge $ M.toList gr
   where mkEdge (k, (v, _)) = L.map (mkLine k) v 
         mkLine a b = line [ lookupPos a gr, lookupPos b gr]
+
+displayMode :: World -> Picture
+displayMode world
+  | putVerticesMode world = translate (-200) 200 $ scale 0.15 0.15 $ text "Left click to put vertices (space when finish)"
+  | putEdgesMode world = translate (-200) 200 $ scale 0.15 0.15 $ text "Click two vertices to join them (space when finish)"
+  | otherwise = blank 
 
 type Graph = M.Map Char [Char] 
 
